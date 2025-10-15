7c54d4485e82d749adf71dd13e72f85780ba2bb3
ComwrapUkReply <s.sznajder@reply.com>
2025-10-15 15:46:33 +0100

    fix: Update placeholders URL in language-switcher.js for AEM authoring
    - Changed the URL for fetching placeholders in AEM authoring from placeholders.html to placeholders.json to ensure correct data retrieval for language mapping.
    - This adjustment aligns with the updated content structure for improved functionality.

--- scripts/language-switcher.js
+++ scripts/language-switcher.js
@@ -108,7 +108,7 @@ async function fetchLanguageMappings() {
     const isAEMAuthoring = pathname.includes('/content/ue-multitenant-root/');
 
     const placeholdersUrl = isAEMAuthoring
-      ? '/content/ue-multitenant-root/placeholders.html'
+      ? '/content/ue-multitenant-root/placeholders.json'
       : '/placeholders.json';
 
     // eslint-disable-next-line no-console
