06143e116204ea0c72e7de196c98b322b3652de6
ComwrapUkReply Szymon <ComwrapUkReply@users.noreply.github.com>
2026-02-03 15:41:14 +0000

    Remove controls attribute from video hero block to streamline user experience

--- blocks/video-hero/video-hero.js
+++ blocks/video-hero/video-hero.js
@@ -95,7 +95,6 @@ export default function decorate(block) {
   video.setAttribute('autoplay', '');
   video.setAttribute('muted', '');
   video.setAttribute('loop', '');
-  video.setAttribute('controls', '');
   video.setAttribute('playsinline', '');
   video.setAttribute('aria-hidden', 'true'); // Decorative video
   video.setAttribute('tabindex', '-1');
