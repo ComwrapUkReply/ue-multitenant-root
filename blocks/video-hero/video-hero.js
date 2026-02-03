0616780ed40bbfb342a1dcb4462ddf7d47b67140
ComwrapUkReply Szymon <ComwrapUkReply@users.noreply.github.com>
2026-02-03 15:40:09 +0000

    Add controls attribute to video hero block for enhanced user interaction

--- blocks/video-hero/video-hero.js
+++ blocks/video-hero/video-hero.js
@@ -95,6 +95,7 @@ export default function decorate(block) {
   video.setAttribute('autoplay', '');
   video.setAttribute('muted', '');
   video.setAttribute('loop', '');
+  video.setAttribute('controls', '');
   video.setAttribute('playsinline', '');
   video.setAttribute('aria-hidden', 'true'); // Decorative video
   video.setAttribute('tabindex', '-1');
